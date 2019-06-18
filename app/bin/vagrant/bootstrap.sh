243dc33f8e7c7ea63b1c4a94227e34a53e89d404
Andy Wallace <Andrew_Wallace@cable.comcast.com>
2019-06-17 21:05:53 -0600

    Updates to build python 3.7.3 in addition to 2.7.5.

--- app/bin/vagrant/bootstrap.sh
+++ app/bin/vagrant/bootstrap.sh
@@ -6,6 +6,24 @@
 #  $ vagrant provision
 #
 ########################################################
+# Add Python based on Option 2: of
+# https://phoenixnap.com/kb/how-to-install-python-3-centos-7
+mkdir /app
+mkdir /app/src
+cd /app/src
+## Download the Python3 version and install separately:
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+## Extract and compile the package:
+tar xzf Python-3.7.3.tgz
+cd Python-3.7.3
+./configure --enable-optimizations
+make altinstall
+cd /usr/bin
+ln -s /app/src/Python-3.7.3/python  python37
+## Verify both python versions:
+python --version
+python37 --version
+########################################################
 # Install website
 cp -R /vagrant/html/* /var/www/html/
 # start apache2
